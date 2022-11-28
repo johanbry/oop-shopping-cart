ff75caf0d18dea6c0e2a129ac286a12b7385d68b
Johan Brynielsson <johan.brynielsson@gmail.com>
2022-11-28 13:28:59 +0100

    product class

--- Product.php
+++ Product.php
@@ -10,6 +10,30 @@ class Product
 
     // TODO Skriv en konstruktor som sätter alla properties
 
+    public function __construct($id, $title, $price, $inStock)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->price = $price;
+        $this->inStock = $inStock;
+    }
+
     // TODO Skriv getters för alla properties
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getInStock() {
+        return $this->inStock;
+    }
 
 }
